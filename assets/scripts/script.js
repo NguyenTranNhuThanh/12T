dbc45a9fd5bb2626c932ba5bb2327da6f1937321
Quackerona <wesofo973@gmail.com>
2024-11-07 20:24:17 +0700

    De-spaghettify + organizing + fixes

---
+++ assets/scripts/script.js
@@ -0,0 +1,32 @@
+const randomizeBtn = document.getElementById('randomizeBtn');
+const resultDiv = document.getElementById('result');
+
+const results = [
+  {
+    text: " Năng lượng tái tạo - Tương lai xanh của nhân loại",
+    image: "/assets/images/drone.jpg"
+  },
+  {
+    text: "Dược phẩm và y tế - Cải thiện chất lượng cuộc sống",
+    image: "/assets/images/hospital.jpg"
+  },
+  {
+    text: "Vật liệu Nano - Tương lai của Công nghiệp",
+    image: "/assets/images/nano.png"
+  },
+  {
+    text: "Công nghệ thực phẩm - Đáp ứng nhu cầu của người tiêu dùng",
+    image: "/assets/images/food.jpg"
+  },
+  
+];
+
+randomizeBtn.addEventListener('click', () => {
+  const randomIndex = Math.floor(Math.random() * results.length);
+  const randomResult = results[randomIndex];
+
+  resultDiv.innerHTML = `
+    <h2 style="color: red">${randomResult.text}</h2>
+    <img src="${randomResult.image}" alt="${randomResult.text}">
+  `;
+});
